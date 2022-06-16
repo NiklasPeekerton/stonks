eb7b1aaada8da1fc0d5dd471870c297a1d0014c1
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-16 20:07:56 +0200

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,24 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
+import pandas as pd
+from pymongo 
+import MongoClient
 
 st.markdown("# Stonkmeizter 🎈")
 st.sidebar.markdown("# Stonkomeizter 🎈")
 
 # streamlit_app.py
 
-import pymongo
-import pandas as pd
-from pymongo import MongoClient
-client = pymongo.MongoClient("mongodb+srv://niklaspee:hEjgok-wepjo5-soqcah@cluster0.80ubl.mongodb.net/?retryWrites=true&w=majority")
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
+
 db = client.stonks
 collection = db.overall
 data = pd.DataFrame(list(collection.find()))
