400671c7e10eb0525db90de7bc2eebc8763c067f
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-16 20:00:53 +0200

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,26 +7,10 @@ st.sidebar.markdown("# Stonkomeizter 🎈")
 # streamlit_app.py
 
 import pymongo
-
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return pymongo.MongoClient(**st.secrets["mongo"])
-
-client = init_connection()
-
-# Pull data from the collection.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def get_data():
-    db = client.stonks
-    items = db.overall.find()
-    items = list(items)  # make hashable for st.experimental_memo
-    return items
-
-items = get_data()
-
-# Print results.
-for item in items:
-    st.write(f"{item['name']} has a :{item['pet']}:")
+import pandas as pd
+from pymongo import MongoClient
+client = pymongo.MongoClient("mongodb+srv://niklaspee:hEjgok-wepjo5-soqcah@cluster0.80ubl.mongodb.net/?retryWrites=true&w=majority")
+db = client.stonks
+collection = db.overall
+data = pd.DataFrame(list(collection.find()))
+data
