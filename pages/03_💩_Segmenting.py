013b621d0563bb8c62a0b8f007d2c5c31129eaac
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-23 20:32:07 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -53,20 +53,65 @@ plost.scatter_chart(
 
 
 
+import echarts
 
 
 
-
-#data = data1.astype(str)
-
-#st.dataframe(df1, width=1500, height=None)
-#test = df2.astype(str)
-
-
-#st.subheader("blö", anchor=None)
-
-#s = data.groupby([' Sector']).mean()
-#mean = s.sort_values(by=['Overall points'], ascending=False)
+options = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Direct", "Mail Ad", "Affiliate Ad", "Video Ad", "Search Engine"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "series": [
+        {
+            "name": "Direct",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [320, 302, 301, 334, 390, 330, 320],
+        },
+        {
+            "name": "Mail Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+            "name": "Affiliate Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+            "name": "Video Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [150, 212, 201, 154, 190, 330, 410],
+        },
+        {
+            "name": "Search Engine",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [820, 832, 901, 934, 1290, 1330, 1320],
+        },
+    ],
+}
+st.echarts(options=options, height="500px")
 
 
 
