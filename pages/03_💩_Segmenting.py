63dc6ffeda528fccbc8260d1dfb3867a639abeef
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-24 00:21:37 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -71,7 +71,7 @@ df = df[['Overall points', 'Ticker', ' Sector','Dividend points normal', 'Revenu
         ]]
 
 df = df.loc[df[' Sector'] == 'Utilities']
-df.drop(' Sector')
+df.drop(columns=[' Sector'])
 st.dataframe(df)
 
 
