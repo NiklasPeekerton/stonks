8b050f42adb6103cdffed55c57adb5607ecb91f1
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-23 20:46:20 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -55,7 +55,7 @@ plost.scatter_chart(
 
 dftrim = dftrim[['Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
      'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal']]
-
+dftrim.set_index(' Sector')
 st.bar_chart(dftrim)
 
 
