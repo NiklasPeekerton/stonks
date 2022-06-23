81472cdf43241f80d59cd85ac9841a99c2c21ed5
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-24 00:43:20 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -76,7 +76,7 @@ df = df.drop(columns=[' Sector'])
 st.dataframe(df)
 
 df = df.sort_values(by=['Overall points'], ascending=True)
-df = df[1:10]
+df = df[10:]
 fig = px.bar(df, x=["Dividend points normal", "Revenues points normal", "Free Cash Flow points normal", 'Net Income points normal', 
                     'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 
                     'Payout Ratio points normal'], y="Ticker", title="Utilities tickers sorted by average overall points broken down my metric",
