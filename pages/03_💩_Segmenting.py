f22a5efcd5b145083e046f08d8e75671cefbe4a2
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-24 00:40:04 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -76,12 +76,12 @@ df = df.drop(columns=[' Sector'])
 st.dataframe(df)
 
 df = df.sort_values(by=['Overall points'], ascending=True)
-df = df[:50]
+df = df[:25]
 fig = px.bar(df, x=["Dividend points normal", "Revenues points normal", "Free Cash Flow points normal", 'Net Income points normal', 
                     'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 
                     'Payout Ratio points normal'], y="Ticker", title="Utilities tickers sorted by average overall points broken down my metric",
             labels=dict(value="Average overall points", variable="Metrics"),
-             height=1000
+             height=800
             )
 st.plotly_chart(fig, use_container_width=True)
 
