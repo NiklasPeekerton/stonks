34887264fca63db1205cde814335b287e4041e2a
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-23 20:42:16 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -53,6 +53,8 @@ plost.scatter_chart(
 
 #gapminder_2002 = gapminder[gapminder['year']==2002]
 
+dftrim = dftrim['Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
+     'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal']
 
 st.bar_chart(dftrim)
 
