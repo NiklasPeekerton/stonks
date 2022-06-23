de367a31a44650c6dd64a88423cc975d3fb4b0c5
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-23 21:53:09 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -71,6 +71,14 @@ df = df[['Overall points', ' Sector','Dividend points normal', 'Revenues points 
         ]]
 st.dataframe(df)
 
+
+import plotly.express as px
+
+#wide_df = px.data.medals_wide()
+
+fig = px.bar(df, x="Overall points", y=["Dividend points normal", "Revenues points normal", "Free Cash Flow points normal"], title="Wide-Form Input")
+fig.show()
+
 sector = alt.Chart(dftrim).mark_bar().encode(
     x='Overall points:Q',
     #y=alt.Y(' Sector:N', sort='-x'),
