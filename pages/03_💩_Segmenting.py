97a8dfeaf4d7acc0b2a43e6a2a1fad6d3817ee6e
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-23 20:20:47 +0200

    Update 03_💩_Segmenting.py

--- pages/03_💩_Segmenting.py
+++ pages/03_💩_Segmenting.py
@@ -34,7 +34,7 @@ plost.bar_chart(
     data=dftrim,
     bar=' Sector',
     value=['Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal'],
-    , 'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'],
+     'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'],
     direction='horizontal')
 
 plost.bar_chart(
