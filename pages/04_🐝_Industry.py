9693a41bc85a061bc0634982246eb3aadeb71109
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-27 18:04:02 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -62,18 +62,7 @@ st.plotly_chart(fig, use_container_width=True)
 df20 = df.head(20)
 df20 = df20.sort_values(by=['Overall points'], ascending=True)
 
-db = client.stonks
-collection = db.overall2
-data = collection.find()
-df = pd.DataFrame(data)
-df = df.drop(columns=['_id'])
-df = df.astype({" Industry": str})
-dfcount = df.groupby(by=' Industry').count()
-dfmean = df.groupby(by=' Industry').mean()
-dfmedian = df.groupby(by=' Industry').median()
-dfmax = df.groupby(by=' Industry').max()
-dfmin = df.groupby(by=' Industry').min()
-#df = df.astype({"Name": str})
+
 
 
 
@@ -88,6 +77,12 @@ def industrymetric(industry):
     df = pd.DataFrame(data)
     df = df.drop(columns=['_id'])
     df = df.astype({" Industry": str})
+    dfcount = df.groupby(by=' Industry').count()
+    dfmean = df.groupby(by=' Industry').mean()
+    dfmedian = df.groupby(by=' Industry').median()
+    dfmax = df.groupby(by=' Industry').max()
+    dfmin = df.groupby(by=' Industry').min()
+    #df = df.astype({"Name": str})
     df = df.sort_values(by=['Overall points'], ascending=False)
     df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
          'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
