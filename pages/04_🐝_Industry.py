847c157d2c3227b6be8f3a86ec05a8581cc1dce5
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-28 14:13:11 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -116,3 +116,10 @@ def industrymetric(industry):
 #  industrymetric(industry)
 industrymetric('Asset Management')
 st.dataframe(df20)
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
