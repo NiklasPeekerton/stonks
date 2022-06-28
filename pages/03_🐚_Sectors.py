ad12d3d1b53abeb0f34326cce5a53532ba9a6c0d
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-28 19:31:47 +0200

    Update 03_🐚_Sectors.py

--- pages/03_🐚_Sectors.py
+++ pages/03_🐚_Sectors.py
@@ -51,12 +51,10 @@ fig.update_xaxes(showgrid=False)
 fig.update_yaxes(showgrid=False)
 st.plotly_chart(fig, use_container_width=True)
 
-#gapminder_2002 = gapminder[gapminder['year']==2002]
-#dftrim = dftrim.set_index(' Sector')
+
 dftrim = dftrim[['Overall points', ' Sector','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
      'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal']]
-#st.dataframe(dftrim)
-#st.bar_chart(dftrim)
+
 
 
 db = client.stonks
@@ -92,32 +90,16 @@ fig = px.bar(df20, x=["Dividend points normal", "Revenues points normal", "Free 
              height=500
             )
 
-def overall():
-    
-    db = client.stonks
-    collection = db.overall2
-    data = collection.find()
-    df = pd.DataFrame(data)
-    df = df.drop(columns=['_id'])
-    df = df.astype({" Sector": str})
-    df = df.sort_values(by=['Overall points'], ascending=False)
-    df = df[['Overall points', 'Name', 'Ticker', ' Sector','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
-         'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
-            ]]
-    return df
 
 
 
 
 def sectormetric(sector):
-    df = overall
     title = st.subheader(sector)
     df = df.loc[df[' Sector'] == sector]
     df = df.drop(columns=[' Sector'])
 
 
-    df20 = df.head(20)
-    df20 = df20.sort_values(by=['Overall points'], ascending=True)
 
 
 
