2bd0d4767e97e6656973c9c2c4c940de08927042
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-28 13:40:06 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -37,7 +37,7 @@ df20 = df20.sort_values(by=['Overall points'], ascending=True)
 fig = px.bar(df20, x=["Dividend points normal", "Revenues points normal", "Free Cash Flow points normal", 'Net Income points normal', 
                     'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 
                     'Payout Ratio points normal'], y=" Industry", title="Industries sorted by average overall points broken down my metric",
-            labels=dict(value="Average overall points", variable="Metrics", height=900)
+            labels=dict(value="Average overall points", variable="Metrics", height=1000)
             )
 st.plotly_chart(fig, use_container_width=True)
 
