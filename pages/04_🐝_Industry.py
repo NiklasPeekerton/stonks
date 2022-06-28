58f866925c90d6d50fefa8995c4036785c4253c7
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-28 14:51:28 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -117,18 +117,18 @@ def industrymetric(industry):
 #st.dataframe(df20)
 
 df = pd.DataFrame(data)
-df = df.drop(columns=['_id'])
+#df = df.drop(columns=['_id'])
 df = df.astype({" Industry": str})
 df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
  'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
     ]]
 
-tickerlist = df['Ticker'].tolist()
+#tickerlist = df['Ticker'].tolist()
 industrylist = df[' Industry'].tolist()
 
 options = st.multiselect(
      'Pick one or several industries you want to see stats for',
-     tickerlist,
+     industrylist,
      #['Yellow', 'Red']
 )
 
