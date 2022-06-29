e81df185fa20b0b67aee9a7a42e4a2a90157fe43
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-29 23:06:42 +0200

    Update 01_💰_Mainstonk.py

--- 01_💰_Mainstonk.py
+++ 01_💰_Mainstonk.py
@@ -22,56 +22,6 @@ client = get_client()
 
 db = client.stonks
 
-#Sorted by value
-collection1 = db.overall1
-data1 = collection1.find()
-df2 = pd.DataFrame(data1)
-df3 = df2.astype({"_id": str})
-df3 = df2.drop(columns=['_id'])
-
-st.subheader("Sorted by value 💰", anchor=None)
-df3=df3[df3!=0].dropna()
-points = df3['Points/Market cap']*df3['Overall points']
-
-df3.insert(3, "Value points", points)
-df3 = df3.sort_values(by=['Value points'], ascending=False)
-df3 = df3.reset_index(drop=True)
-st.dataframe(df3)
-
-
-collection = db.overall
-data = collection.find()
-
-
-df = pd.DataFrame(data)
-df = df.astype({"_id": str})
-df1 = df.drop(columns=['_id'])
-#st.dataframe(df1, width=1500, height=None)
-test = df1.astype(str)
-
-#st.subheader("'New' scores", anchor=None)
-#df4 = pd.DataFrame(data1)
-#df5 = df4.astype({"_id": str})
-#df5 = df4.drop(columns=['_id'])
-
-#df4=df4[df4!=0].dropna()
-#st.dataframe(df4)
-
-st.subheader("'Old' scores", anchor=None)
-st.dataframe(test)
-test1 = test[:500]
-
-
-collection1 = db.overall1
-data1 = collection1.find()
-df2 = pd.DataFrame(data1)
-
-df3 = df2.astype({"_id": str})
-df3 = df2.drop(columns=['_id'])
-
-
-
-
 
 
 @st.experimental_memo
@@ -94,4 +44,9 @@ def giveme(coll):
     return df, df20
 
 
-giveme(df20)
+#giveme(overall2)
+
+full = giveme(overall2)[0]
+top20 = giveme(overall2)[1]
+
+st.dataframe(top20)
