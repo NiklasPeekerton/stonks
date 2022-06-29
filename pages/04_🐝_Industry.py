2e63cc15e9ade3b04fb1bd61bfb254d958f6eec4
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-29 17:46:24 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -69,20 +69,11 @@ def industrymetric(industry):
     collection = db.overall2
     overall = collection.find({' Industry': industry })
     df = pd.DataFrame(overall)
-    #df = df.drop(columns=['_id'])
     df = df.astype({" Industry": str})
     df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
      'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
         ]]
-    #dfcount = df.groupby(by=' Industry').count()
-    #dfmean = df.groupby(by=' Industry').mean()
-    #dfmedian = df.groupby(by=' Industry').median()
-    #dfmax = df.groupby(by=' Industry').max()
-    #dfmin = df.groupby(by=' Industry').min()
-    #df = df.sort_values(by=['Overall points'], ascending=False)
-    
 
-    title = st.subheader(industry)
     df = df.loc[df[' Industry'] == industry]
     df = df.drop(columns=[' Industry'])
 
@@ -90,41 +81,21 @@ def industrymetric(industry):
     df20 = df.head(25)
     df20 = df20.sort_values(by=['Overall points'], ascending=True)
 
-
-
-    fig = px.bar(df20, x=["Dividend points normal", "Revenues points normal", "Free Cash Flow points normal", 'Net Income points normal', 
-                        'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 
-                        'Payout Ratio points normal'], y="Ticker", title="Utilities tickers sorted by average overall points broken down my metric", text='Overall points',
-                labels=dict(value="Average overall points", variable="Metrics"),
-                 height=600
-                )
-
-
-
-    #col1, col2, col3, col4, col5 = st.columns(5)
-    #col1.metric("Number of stocks", dfcount.loc[industry][0])#, "1.2 °F")
-    #col2.metric("Average score", dfmean.loc[industry][0])#, "-8%")
-    #col3.metric("Median score", dfmedian.loc[industry][0])#, "4%")
-    #col4.metric("Max score", dfmax.loc[industry][0])#, "4%")
-    #col5.metric("Min score", dfmin.loc[industry][0])#, "4%")
     df = df.astype({"Name": str})
 
-    st.plotly_chart(fig, use_container_width=True)
-    #df = df.sort_values(by=['Overall points'], ascending=False)
-    st.dataframe(df)
-    return fig#, col1,col2,col3,col4,col5#,df#,dfcount,dfmean,dfmedian,dfmax,dfmin
+    return df, df20
 
 #for industry in df20[' Industry']:
 #  industrymetric(industry)
 #industrymetric('Asset Management')
 #st.dataframe(df20)
 
-df = pd.DataFrame(overall)
+#df = pd.DataFrame(overall)
 #df = df.drop(columns=['_id'])
-df = df.astype({" Industry": str})
-df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
- 'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
-    ]]
+#df = df.astype({" Industry": str})
+#df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal', 'Revenues points normal', 'Free Cash Flow points normal', 'Net Income points normal',
+# 'Net Income Margin points normal', 'Current Ratio points normal', 'Weighted Average Shares (Diluted) points normal', 'Payout Ratio points normal'
+#    ]]
 
 #tickerlist = df['Ticker'].tolist()
 #dfcount = df.groupby(by=' Industry').count()
@@ -140,4 +111,5 @@ df = df[['Overall points', 'Name', 'Ticker', ' Industry','Dividend points normal
 #st.write('You selected:', options)
 #print(options)
 #print(type(options))
-industrymetric('Railroads')
+test = industrymetric('Railroads')[0]
+st.write(test)
