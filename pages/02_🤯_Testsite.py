0bfa25dc3b195a6d2b8f080271ded95b11fc809d
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 21:53:14 +0200

    Update 02_🤯_Testsite.py

--- pages/02_🤯_Testsite.py
+++ pages/02_🤯_Testsite.py
@@ -81,9 +81,7 @@ full['Current Ratio points normal'] = full['Current Ratio points normal']*weight
 full['Weighted Average Shares (Diluted) points normal'] = full['Weighted Average Shares (Diluted) points normal']*weight
 full['Payout Ratio points normal'] = full['Payout Ratio points normal']*weight
 
-full['Overall points'] = full['Overall points'], 
-                        + (full['Revenues points normal']-(full['Revenues points normal']/weight)), 
-                        + (full['Dividend points normal']-(full['Dividend points normal']/weight))
+full['Overall points'] = full['Overall points']+(full['Revenues points normal']-(full['Revenues points normal']/weight)) + (full['Dividend points normal']-(full['Dividend points normal']/weight)) + (full['Free Cash Flow points normal']-(full['Free Cash Flow points normal']/weight)) + (full['Net Income points normal']-(full['Net Income points normal']/weight)) + (full['Net Income Margin points normal']-(full['Net Income Margin points normal']/weight)) + (full['Current Ratio points normal']-(full['Current Ratio points normal']/weight)) + (full['Weighted Average Shares (Diluted) points normal']-(full['Weighted Average Shares (Diluted) points normal']/weight)) + (full['Payout Ratio points normal']-(full['Payout Ratio points normal']/weight)
 
 full = full.sort_values(by=['Overall points'], ascending=False)
 full = full.reset_index(drop=True)
