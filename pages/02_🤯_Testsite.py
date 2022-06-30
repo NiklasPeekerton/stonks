2ff10fc4795b6f474f6084e56184e991fa35b675
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 21:17:21 +0200

    Update 02_🤯_Testsite.py

--- pages/02_🤯_Testsite.py
+++ pages/02_🤯_Testsite.py
@@ -31,7 +31,7 @@ data = collection.find()
 df = pd.DataFrame(data)
 df = df.drop(columns=['_id'])
 df = df.drop(columns=[' CUSIP'])
-df = df.drop(columns=[' CIK'])
+df = df.drop(columns=['CIK'])
 df = df.astype({" IPO Date": str})
 
 weight = st.slider('Weight for Revenue', 1, 10, 1)
