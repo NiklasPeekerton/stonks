5160b98ed50c3fe38e4a6319342ce4e1009449a0
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 13:12:02 +0200

    Update 03_🐚_Sectors.py

--- pages/03_🐚_Sectors.py
+++ pages/03_🐚_Sectors.py
@@ -129,8 +129,8 @@ test = full.style.format({"Market Capitalization size": "${:20,.0f}"})\
                  .bar(subset=["Net Income Margin points normal"], color='#FFA15A')\
                  .bar(subset=["Current Ratio points normal"], color='#19D3F3')\
                  .bar(subset=["Weighted Average Shares (Diluted) points normal"], color='#FF6692')\
-                 .bar(subset=["Payout Ratio points normal"], color='#B6E980')\
-                 .background_gradient(cmap='Blues')
+                 .bar(subset=["Payout Ratio points normal"], color='#B6E980')
+                 #.background_gradient(cmap='Blues')
                      
 #st.table(test)
 st._legacy_dataframe(test)
