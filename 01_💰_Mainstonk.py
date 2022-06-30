28c7abc7aa34d7533c65da591e215512bfc3a64a
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 18:04:37 +0200

    Update 01_💰_Mainstonk.py

--- 01_💰_Mainstonk.py
+++ 01_💰_Mainstonk.py
@@ -95,7 +95,7 @@ st.plotly_chart(valuepoints, use_container_width=True)
 
 st._legacy_dataframe(test, height=800)
 
-multiplots = px.scatter(full, x="Market Capitalization size", y="Overall points", facet_col=" Sector", facet_col_wrap=4, height=1000)
+multiplots = px.scatter(full, x="Market Capitalization size", y="Overall points", facet_col=" Sector", facet_col_wrap=2, height=1000)
 multiplots.update_xaxes(title_font=dict(size=18, family='Courier', color='crimson'))
 multiplots.update_yaxes(title_font=dict(size=18, family='Courier', color='crimson'))
 
