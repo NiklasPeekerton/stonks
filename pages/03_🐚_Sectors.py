a2f9d07a1727eb456b8800475a0677658e75b3be
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 13:30:24 +0200

    Update 03_🐚_Sectors.py

--- pages/03_🐚_Sectors.py
+++ pages/03_🐚_Sectors.py
@@ -118,8 +118,7 @@ col6.metric("50% score", test.loc['50%'][0])
 col7.metric("75% score", test.loc['75%'][0])
     
 st.plotly_chart(fig, use_container_width=True)
-test = full.style.format({"Market Capitalization size": "${:20,.0f}"})\
-                 ({"Overall points": int}, precision=None)\
+test = full.style.format({"Market Capitalization size": "${:20,.0f}"}, {"Overall points": int}, precision=None)\
                  .hide_index()\
                  .bar(subset=["Overall points"], color='1B2432')\
                  .bar(subset=["Market Capitalization size"], color='lightgreen')\
