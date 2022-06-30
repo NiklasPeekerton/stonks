1943e376484bda2de8f49b40254afc40b20f9b8e
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 19:40:01 +0200

    Update 04_🐝_Industry.py

--- pages/04_🐝_Industry.py
+++ pages/04_🐝_Industry.py
@@ -49,10 +49,19 @@ fig = px.scatter(df20, x="Overall points", y="Market Capitalization size", color
                  #width=800, 
                  height=900
                 )
+valuepoints = px.scatter(full, x="Market Capitalization size", y="Overall points", color=' Industry', 
+                         log_y=True, log_x=True, trendline="ols", trendline_scope="overall", #text=' Sector',
+                 title="Log scale of market cap by overall points. The size of the bubbles are based on the Free cash flow points",
+                labels=dict(value="Average market Capitalization size", y="Average overall points"),
+                 #width=800, 
+                 height=900,
+                 size='Free Cash Flow points normal',
+                 hover_name="Name"
+                )
 
-fig.update_xaxes(showgrid=False)
-fig.update_yaxes(showgrid=False)
-st.plotly_chart(fig, use_container_width=True)
+valuepoints.update_xaxes(showgrid=False)
+valuepoints.update_yaxes(showgrid=False)
+st.plotly_chart(valuepoints, use_container_width=True)
 
 
 
