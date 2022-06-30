7bd32e1878077f86b523a367e056e4247982b191
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-06-30 20:48:23 +0200

    Update 03_🐚_Sectors.py

--- pages/03_🐚_Sectors.py
+++ pages/03_🐚_Sectors.py
@@ -43,15 +43,10 @@ st.plotly_chart(fig, use_container_width=True)
 
 
 
-fig = px.scatter(df, x="Overall points", y="Market Capitalization size", color=' Sector', log_y=True, log_x=True, text=' Sector',
-                 title="Log scale of market cap by overall points",
-                labels=dict(value="Average market Capitalization size", y="Average overall points"),
-                 #width=800, 
-                 height=900
-                )
+
 
 valuepoints = px.scatter(df, x="Market Capitalization size", y="Overall points", color=' Sector', 
-                         log_y=True, log_x=True, trendline="ols", trendline_scope="overall", #text=' Sector',
+                         log_y=True, log_x=True, trendline="ols", trendline_scope="overall", text=' Sector',
                  title="Log scale of market cap by overall points. The size of the bubbles are based on the Free cash flow points",
                 labels=dict(value="Average market Capitalization size", y="Average overall points"),
                  #width=800, 
@@ -60,9 +55,7 @@ valuepoints = px.scatter(df, x="Market Capitalization size", y="Overall points",
                  hover_name=" Sector"
                 )
 
-fig.update_xaxes(showgrid=False)
-fig.update_yaxes(showgrid=False)
-st.plotly_chart(fig, use_container_width=True)
+
 
 valuepoints.update_xaxes(type="log", range=[np.log10(80), np.log10(11000000000)])
 valuepoints.update_yaxes(type="log", range=[np.log10(80), np.log10(600)])
