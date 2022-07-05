c126233b0286c95be1a9ec89916b5aff8de51c88
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-07-05 17:31:16 +0200

    Update 01_💰_Mainstonk.py

--- 01_💰_Mainstonk.py
+++ 01_💰_Mainstonk.py
@@ -101,7 +101,7 @@ options = st.selectbox(
 )
 
 
-valuepoints = px.scatter(full, x=options, y="Overall points", color=' Sector', 
+valuepoints = px.scatter(full1, x=options, y="Overall points", color=' Sector', 
                          log_y=True, log_x=False, trendline="ols", trendline_scope="overall", #text=' Sector',
                          trendline_options=dict(log_x=False, log_y=True), 
                          title="Log scale of market cap by overall points. The size of the bubbles are based on the Free cash flow points",
@@ -113,6 +113,7 @@ valuepoints = px.scatter(full, x=options, y="Overall points", color=' Sector',
                 )
 
 #valuepoints.update_xaxes(type="log", range=[np.log10(598600000000), np.log10(598650000000)])
+valuepoints.update_xaxes(range=[-6, 6])
 valuepoints.update_yaxes(type="log", range=[np.log10(80), np.log10(1000)])
 st.plotly_chart(valuepoints, use_container_width=True)
 
